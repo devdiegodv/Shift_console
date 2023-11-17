300ede6f8a65621623418f0c5cb96ac39c9fae74
dev-diegov <diegoidv.us@gmail.com>
2023-11-17 20:22:36 -0300

    change f to ph

--- shift_console.py
+++ shift_console.py
@@ -1,6 +1,11 @@
 import numbers
 
 def ask():
+    """
+    Function to prompt the user to choose a category (Perfumery, Pharmacy, or Cosmetics).
+    It uses a while loop to ensure a valid category is selected.
+    After a valid category is chosen, it calls the decorator function from the 'numbers' module.
+    """
     print('Welcome to the Pharmacy')
 
     while True:
@@ -14,3 +19,23 @@ def ask():
             break
     
     numbers.decorator(my_category)
+
+def start():
+    """
+    Function to start the interaction with the user.
+    It repeatedly calls the 'ask' function and asks the user if they want to take another turn.
+    If the user chooses not to take another turn, the loop breaks, and a farewell message is printed.
+    """
+    while True:
+        ask()
+        try:
+            another_turn = input('Do you want to take another turn? [Y] or [N]: ').upper()
+            ['Y', 'N'].index(another_turn)
+        except ValueError:
+            print('That is not a valid option')
+        else:
+            if another_turn == 'N':
+                print('Thanks for visiting')
+                break
+
+start()
